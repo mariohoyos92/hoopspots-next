6e52754d48b5a1904ea2c1d303f9f55cf0f116f4
Mario Hoyos <mariohoyos92@gmail.com>
2020-08-29 16:29:29 -0700

    fix: broken mongo connection

--- pages/api/launch-signup/index.ts
+++ pages/api/launch-signup/index.ts
@@ -1,8 +1,10 @@
 import LaunchSignupModel from '../_models/launch-signup-model';
+import connectToMongo from '../_database-connections/mongoose-connection';
 
 export default async function launchSignup(req, res) {
   try {
     if (req.method === 'POST') {
+      await connectToMongo();
       await LaunchSignupModel.create({ email: req.body.email });
       res.status(201).end();
     }
